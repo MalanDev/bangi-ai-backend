2d3a94bb4da5f6f3f11c04eaf1f7bb12c8c9a4c3
MalanDev <malandev95@gmail.com>
2024-06-06 17:49:35 +0530

    enable cors

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const imageRoutes = require('./routes/imageRoutes');
+var cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use('/auth', authRoutes);
 app.use('/api', imageRoutes);
