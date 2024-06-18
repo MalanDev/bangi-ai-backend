edce66135aa61f109773ba5248bfe38bb4bf01c5
MalanDev <malandev95@gmail.com>
2024-06-18 11:39:08 +0530

    change to droplet server

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'bn6vws8zpfpngkxypnmh-mysql.services.clever-cloud.com',
-  user: 'urmjk4uuterpxmkr',
-  password: 'oY1X6uFV193RbUUQD07U',
-  database: 'bn6vws8zpfpngkxypnmh',
+  host: 'localhost',
+  user: 'root',
+  password: 'Root@123',
+  database: 'banigi_ai',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
